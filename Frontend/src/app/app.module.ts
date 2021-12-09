eff3d90834464b9739131c350b8d7b9420e594fe
12iyad <85706501+12iyad@users.noreply.github.com>
2021-12-09 12:48:08 +0000

    Fixed issue of properties of not disappearing when need to

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -11,13 +11,20 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+
+
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
+  {path: 'user-login', component: UserLoginComponent},
+  {path: 'user-register', component: UserRegisterComponent},
   {path: '**', component: PropertyListComponent}
+
 ]
 
 @NgModule({
@@ -25,9 +32,11 @@ const appRoutes: Routes = [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-      NavBarComponent,
-      AddPropertyComponent,
-      PropertyDetailComponent
+    NavBarComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent
    ],
   imports: [
     BrowserModule,
